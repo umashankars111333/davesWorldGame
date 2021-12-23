e6838328f71d942c3629d14dd8ba6ae54e6b3762
Umashankar S <Umashankar.S@finastra.com>
2021-12-23 19:38:33 +0530

    port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/getData", function (req, res) {
   res.send(JSON.stringify(jsonData));
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, function () {
   console.log("Listening on port 3000");
 });
